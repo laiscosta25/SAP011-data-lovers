1725d7ab149b7f92b136eb46e395648290d95cbf
laboratoria <ariadne.semiao1910@gmail.com>
2023-08-25 15:32:00 -0300

    Filtros e busca funcionando

--- src/data.js
+++ src/data.js
@@ -1,9 +1,61 @@
-// estas funciones son de ejemplo
+const dataFunctions = {
+  filter: function (characters, value, key) {
+    const filter = characters.filter(function (character) {
+      const filtered = character[key].includes(value);
 
-export const example = () => {
-  return 'example';
+      return filtered;
+    });
+
+    return filter;
+  },
+
+  ascending: function (characters) {
+    const order = characters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return order;
+  },
+
+  descending: function (characters) {
+    const order = characters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    
+    return order;
+  },
+
+  order: function (characters, selectAz) {
+    const copy = [...characters];
+    if (selectAz === "A-Z") {
+      return dataFunctions.ascending(copy);
+    } 
+    return dataFunctions.descending(copy);
+  },
+
+  
+
+  searchName: function (characters, name) {
+    const filter = characters.filter(function (character) {
+      const filteredSearch = character.name
+      .toLowerCase()
+      .includes(name.toLowerCase());
+    return filteredSearch;
+    });
+    return filter;
+  },
+
+  calculatePercentage: function (sizeList, sizeFilteredList) {
+    return Math.round((sizeFilteredList * 100) / sizeList);
+  },
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export default dataFunctions;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,86 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from "./data/breakingbad/breakingbad.js"; 
+import dataFunctions from "./data.js";
+
+const characters = data.breaking_bad;
+const cardContainer = document.querySelector("#cards-container");
+const selectStatus = document.querySelector("#status");
+const selectSerie= document.querySelector("#serie");
+const search = document.querySelector("#search"); //cria busca por nome.
+const percentageElement = document.querySelector("#percentage");
+const selectAz = document.querySelector("#a-z");
+
+function displayCards(characters) {
+  const cardsResult = characters.map((item) => {
+    const template = `
+      <div class="card">
+
+      <img class="poster-img" src="${item.img}" alt="${item.name}">
+        <ul class="card-text" style="list-style: none">
+          <li>Name: ${item.name}</li>
+          <li>Birthday: ${item.birthday}</li>
+          <li>Nickname: ${item.nickname}</li>
+          <li>Occupation: ${item.occupation}</li>
+          <li>Status: ${item.status}</li>
+          <li>Appearance: ${item.appearance}</li>
+          <li>Portrayed: ${item.portrayed}</li>
+          <li>Category: ${item.category}</li>   
+        </ul>
+      </div>
+`;
+
+return template;
+});
+
+return cardsResult.join("");
+}
+
+cardContainer.innerHTML = displayCards(characters);
+
+selectAz.addEventListener("change", (event) => {
+  const value = event.target.value; //extrai o valor do elemento específico que acionou o evento
+  const orderedList = dataFunctions.order(characters, value);
+  const cards = displayCards(orderedList);
+  cardContainer.innerHTML = cards;
+});
+
+selectStatus.addEventListener("change", (event) => {
+  const value = event.target.value;
+  const filteredList = dataFunctions.filter(characters, value, "status");
+  const cards = displayCards(filteredList);
+  cardContainer.innerHTML = cards;
+
+  const percentage = dataFunctions.calculatePercentage(
+    characters.length,
+    filteredList.length
+  );
+
+  percentageElement.innerHTML = 
+    "Essa categoria corresponde a " + percentage + "% dos personagens."
+    
+});
+
+selectSerie.addEventListener("change", (event) => {
+  const value = event.target.value;
+  const filteredList = dataFunctions.filter(characters, value, "category");
+  const cards = displayCards(filteredList);
+  cardContainer.innerHTML = cards;
+
+  const percentage = dataFunctions.calculatePercentage(
+    characters.length,
+    filteredList.length
+  );
+
+  percentageElement.innerHTML = 
+    "Essa categoria corresponde a " + percentage + "% dos personagens."
+});
+
+search.addEventListener("keyup", function (event) {
+  const value = event.target.value;
+  const filteredList = dataFunctions.searchName(characters, value);
+  const cards = displayCards(filteredList);
+  cardContainer.innerHTML = cards;
+});
+
+
+
